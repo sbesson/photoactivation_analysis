59eeecd31a8284fe93ebe5f0d44087cebbca61d4
Sebastien Besson <sebastien.besson@ed.ac.uk>
2012-12-24 14:27:23 +0000

    Generate bar graphs for final spindle length and elongation rate
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/biology/welburnlab@15 23d1e5db-ab4f-45ea-9093-01c07e8c706e

--- spindleAnalysis/runSpindleAnalysis.m
+++ spindleAnalysis/runSpindleAnalysis.m
@@ -23,6 +23,13 @@ fileLog = cell(nConditions, 1);
 minSpindleLength = 6; % minimum spindle length (in microns)
 sigma = 3; % Sigma to exclude outliers (using final spindle length)
 
+% Define mapping for bar graphs
+graph_map{1,1} = 'elongationRate';
+graph_map{1,2} = 'Elongation rate (microns/min)';
+graph_map{2,1} = 'finalLengths';
+graph_map{2,2} = 'Final spindle length (microns)';
+nGraphs = size(graph_map, 1);
+
 for iCondition = 1:nConditions
     
     % Find Excel file in condition folder
@@ -32,7 +39,7 @@ for iCondition = 1:nConditions
         conditionPath);
     xlsfile = fullfile(conditionPath, xlsfiles(1).name);
     
-    %%
+    %% Read data
     fileLog{iCondition} = sprintf('Reading file %s...\n', xlsfile);
     
     % Read number of worksheets
@@ -65,7 +72,7 @@ for iCondition = 1:nConditions
         %% Outlier detection
         
         % Compute final spindle lengths
-        finalLengths = NaN(size(data,2), 1);
+        finalLengths = NaN(1, size(data,2));
         for i = 1:size( data,2)
             finalLengths(i) = data(find(~isnan( data(:,i)), 1, 'last'), i);
         end
@@ -88,14 +95,12 @@ for iCondition = 1:nConditions
         conditions(iCondition).series(iSeries).finalLengths = finalLengths;
         conditions(iCondition).series(iSeries).data = data;
         
-        
         % Log number of time points and samples
         nTimePoints = size(data,1);
         nSamples = size(data,2);
         log1 = sprintf('Number of timepoints: %g\n', nTimePoints);
         log2 = sprintf('Number of samples: %g\n', nSamples);
-        seriesLog{iSeries} = [seriesLog{iSeries} log1 log2];
-        
+        seriesLog{iSeries} = [seriesLog{iSeries} log1 log2]; 
         
         %% Detect events
         [events, eventlog] = detectEvents(data, times, minSpindleLength);
@@ -131,9 +136,20 @@ for iCondition = 1:nConditions
         close(gcf)
     end
     
-    % Aggregate scalar measurements
-    conditions(iCondition).elongationRate = ...
-        horzcat(conditions(iCondition).series.elongationRate);
+    %% Create comparative graphs per condition
+    
+    for iGraph = 1 : nGraphs
+        % Aggregate scalar data
+        conditions(iCondition).(graph_map{iGraph, 1}) = ...
+            horzcat(conditions(iCondition).series.(graph_map{iGraph, 1}));
+        
+        % Plot summary bar graphs
+        figure;
+        plotBarGraphs({conditions(iCondition).series.(graph_map{iGraph, 1})},...
+            {conditions(iCondition).series.name}, graph_map{iGraph, 2});
+        print(gcf,'-dtiff',fullfile(conditionPath, [graph_map{iGraph, 1} '.tif']));
+        close(gcf);
+    end
     
     % Pool aligned data
     [alignedTimes, alignedData] = ...
@@ -141,25 +157,33 @@ for iCondition = 1:nConditions
     conditions(iCondition).alignedTimes = alignedTimes;
     conditions(iCondition).alignedData = alignedData;
     
-    % Display and save full log
-    disp([fileLog{iCondition} seriesLog{:}])
-    fid = fopen(fullfile(conditionPath, 'Summary-log.txt'), 'w');
-    fprintf(fid, [fileLog{iCondition} seriesLog{:}]);
-    fclose(fid);
-    
     % Set summary figure options
     conditionFig = figure();
     plotMeanAlignedData(conditions(iCondition).series)
     xlim = get(gca, 'XLim');
     plot([xlim(1) xlim(2)], [minSpindleLength minSpindleLength] ,'--k');
     print(gcf,'-dtiff',fullfile(conditionPath, 'AlignedData.tif'));
-    close(conditionFig);
+    close(conditionFig);   
+
+    % Display and save full log
+    disp([fileLog{iCondition} seriesLog{:}])
+    fid = fopen(fullfile(conditionPath, 'Summary-log.txt'), 'w');
+    fprintf(fid, [fileLog{iCondition} seriesLog{:}]);
+    fclose(fid);
 end
 
 % Save output
 save(fullfile(dataPath, 'analysis.mat'), 'conditions');
 
-%% Generate conditions graph
+%% Generate comparative graphs between conditions
+
+for iGraph = 1 : nGraphs
+    figure;
+    plotBarGraphs({conditions.(graph_map{iGraph, 1})},...
+        {conditions.name}, graph_map{iGraph, 2});
+    print(gcf,'-dtiff',fullfile(dataPath, [graph_map{iGraph, 1} '.tif']));
+    close(gcf);
+end
 
 % Generated all conditions comparison
 allConditionsFig = figure();
