2641183c5a51e1d0c8504a69485c3b4a5170d41a
Sebastien Besson <sebastien.besson@ed.ac.uk>
2013-04-18 12:46:36 +0000

    Fix figure names in exportFigure()
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/biology/welburnlab@28 23d1e5db-ab4f-45ea-9093-01c07e8c706e

--- spindleAnalysis/exportFigure.m
+++ spindleAnalysis/exportFigure.m
@@ -7,5 +7,5 @@ f(2).driver = '-depsc';
 f(2).ext = 'eps';
 
 for i = 1 : numel(f)
-    print(fig_handle, f(i).driver, fullfile(path, [basename f(i).ext]));
+    print(fig_handle, f(i).driver, fullfile(path, [basename '.' f(i).ext]));
 end
