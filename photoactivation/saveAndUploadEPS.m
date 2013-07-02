964dc05be44b6453d4d6bb93503072c8786dc1fd
Sebastien Besson <sebastien.besson@ed.ac.uk>
2013-07-02 13:57:29 +0000

    Refactor the original file update logic to allow multiple tries
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/biology/welburnlab@41 23d1e5db-ab4f-45ea-9093-01c07e8c706e

--- photoactivation/saveAndUploadEPS.m
+++ photoactivation/saveAndUploadEPS.m
@@ -4,11 +4,19 @@ fullPath = fullfile(path, [filename '_' num2str(id) '.eps']);
 print(fig, '-depsc', fullPath);
 close(fig);
 
+nTries = 5;
+
 % Save projections onto the server
 fa = getImageFileAnnotations(session, id, 'include', ns);
 if isempty(fa),
     fa = writeFileAnnotation(session, fullPath, 'namespace', ns);
+    fprintf(1, 'Created file annotation %g ', fa.getId().getValue());
     linkAnnotation(session, fa, 'image', id);
+    fprintf(1, 'and linked it to image %g\n', id);
 else
-    updateFileAnnotation(session, fa, fullPath);
+    originalFile =  fa.getFile();
+    fprintf(1, 'Updating file: %g\n', originalFile.getId().getValue());
+    fprintf(1, 'Old SHA1: %s\n', char(originalFile.getSha1().getValue()));
+    originalFile = updateOriginalFile(session, originalFile, fullPath, nTries);
+    fprintf(1, 'New SHA1: %s\n', char(originalFile.getSha1().getValue()));
 end
---
+++ photoactivation/updateOriginalFile.m
@@ -0,0 +1,96 @@
+function originalFile = updateOriginalFile(session, originalFile, filePath, varargin)
+% UPDATEORIGINALFILE Replace a file with new content on the OMERO server
+%
+%    originalFile = updateOriginalFile(session, originalFile, filePath)
+%    replaces the content of the original file specified by filePath
+%
+%    originalFile = updateOriginalFile(session, originalFile, filePath,
+%    nTries) also specifies the number of tries of the upload fails.
+%
+%    Examples:
+%
+%        originalFile = updateOriginalFile(session, originalFile, filePath)
+%        originalFile = updateOriginalFile(session, originalFile, filePath, nTries)
+%
+% See also: createFileAnnotation
+
+% Copyright (C) 2013 University of Dundee & Open Microscopy Environment.
+% All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+% Input check
+ip = inputParser;
+ip.addRequired('session');
+ip.addRequired('originalFile', @(x) isa(x, 'omero.model.OriginalFile'));
+ip.addRequired('filePath', @(x) exist(x, 'file') == 2);
+ip.addOptional('nTries', 1, @isscalar);
+ip.parse(session, originalFile, filePath, varargin{:});
+
+% 
+[~, f] = fileattrib(filePath);
+absolutePath = f.Name;
+
+% Compare checksums client-side and server-sid
+[originalFile, hasher] = updateFile(session, originalFile, absolutePath);
+clientHash = char(hasher.checksumAsString());
+serverHash = char(originalFile.getSha1().getValue());
+iTry = 1;
+
+while iTry < ip.Results.nTries && ~strcmp(clientHash, serverHash)
+    disp('File checksum mismatch on upload. Retrying...');
+    iTry = iTry + 1;
+    [originalFile, hasher] = updateFile(session, originalFile, absolutePath);
+    clientHash = char(hasher.checksumAsString());
+    serverHash = char(originalFile.getSha1().getValue());
+end
+
+msg = 'File checksum mismatch on upload: %s (client has %s, server has %s)';
+assert(isequal(clientHash, serverHash), msg, filePath, clientHash, serverHash);
+
+function [originalFile, hasher] = updateFile(session, originalFile, absolutePath)
+
+% Create java io File
+[path, name, ext] = fileparts(absolutePath);
+fileLength = length(java.io.File(absolutePath));
+
+% Create original file
+originalFile.setName(rstring([name ext]));
+originalFile.setPath(rstring(path));
+originalFile.setSize(rlong(fileLength));
+
+% now we save the originalFile object
+updateService = session.getUpdateService();
+originalFile = updateService.saveAndReturnObject(originalFile);
+
+% Initialize provider to compute client-side checksum
+checksumProviderFactory = ome.util.checksum.ChecksumProviderFactoryImpl;
+sha1= ome.util.checksum.ChecksumType.SHA1;
+hasher = checksumProviderFactory.getProvider(sha1);
+
+% Initialize the service to load the raw data
+rawFileStore = session.createRawFileStore();
+rawFileStore.setFileId(originalFile.getId().getValue());
+
+%code for small file.
+fid = fopen(absolutePath);
+byteArray = fread(fid,[1, fileLength], 'uint8');
+rawFileStore.write(byteArray, 0, fileLength);
+hasher.putBytes(byteArray);
+fclose(fid);
+
+% Save and close the service
+originalFile = rawFileStore.save();
+rawFileStore.close();
