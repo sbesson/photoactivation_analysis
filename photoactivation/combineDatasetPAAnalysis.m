e2c697b04709cd225d7dcbc291ccedf579478708
Sebastien Besson <sebastien.besson@ed.ac.uk>
2014-03-14 19:40:45 +0000

    Adjustements for final analysis
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/biology/welburnlab@76 23d1e5db-ab4f-45ea-9093-01c07e8c706e

--- photoactivation/combineDatasetPAAnalysis.m
+++ photoactivation/combineDatasetPAAnalysis.m
@@ -62,23 +62,26 @@ all_positions(:, negative_slopes) = - all_positions(:, negative_slopes);
 
 
 % Create filters
-longtimes_filter = [data.t2]  < 1e10;
-r2_filter_0 = [data.r2] >= 0;
-r2_filter_90 = [data.r2] >= .9;
-r2_filter_95 = [data.r2] >= .95;
-filters(1).name = 'Unfiltered';
-filters(1).values = ones(size([data.t2]));
-filters(2).name = 'Long times';
-filters(2).values = longtimes_filter & r2_filter_0;
-filters(3).name = 'Long times & .90';
-filters(3).values = longtimes_filter & r2_filter_90;
-filters(4).name = 'Long times & .95 ';
-filters(4).values = longtimes_filter & r2_filter_95;
-filters(5).name = '.90 ';
-filters(5).values = r2_filter_90;
-filters(6).name = '.95 ';
-filters(6).values = r2_filter_95;
-
+% longtimes_filter = [data.t2]  < 1e10;
+% r2_filter_0 = [data.r2] >= 0;
+% r2_filter_80 = [data.r2] >= .8;
+% r2_filter_90 = [data.r2] >= .9;
+% r2_filter_95 = [data.r2] >= .95;
+% filters(1).valu = 'Unfiltered';
+% filters(1).values = ones(size([data.t2]));
+% % filters(2).name = 'Long times';
+% % filters(2).values = longtimes_filter & r2_filter_0;
+% % filters(3).name = 'Long times & .90';
+% % filters(3).values = longtimes_filter & r2_filter_90;
+% % filters(4).name = 'Long times & .95 ';
+% % filters(4).values = longtimes_filter & r2_filter_95;
+% filters(4).value = '.80 ';
+% filters(5).name = '.90 ';
+% filters(5).values = r2_filter_90;
+% filters(6).name = '.95 ';
+% filters(6).values = r2_filter_95;
+% filters = filters([1 5 6]);
+filters = [.9];
 % Average and fit the normalized intensities
 dataTypes(1).name = 'uncorrected';
 dataTypes(1).field = 'dInorm';
@@ -86,9 +89,9 @@ if isfield(data, 'dInorm_corr')
     dataTypes(2).name = 'corrected';
     dataTypes(2).field = 'dInorm_corr';
 end
-maxTimes = [200, 300];
+maxTimes = [300];
 logmsg = 'Writing results for %s data of dataset %g with a cutoff of %gs\n';
-for iType = 1 : numel(dataTypes)
+for iType = 2 : numel(dataTypes)
     for maxTime = maxTimes
 
         % Create results table
@@ -99,13 +102,15 @@ for iType = 1 : numel(dataTypes)
         fprintf(fid, ['Name\tId\tFull-range speed (microns/min)\t'...
             'Half-range speed (microns/min)\t'...
             'Fast turnover time (s)\tSlow turnover time (s)\tr2\n']);
+        r2 = NaN(size(validData));
         for i = find(validData)',
-            [bFit, r2] = fitPADecay(data(i).times,...
+            [bFit, r2(i)] = fitPADecay(data(i).times,...
                 data(i).(dataTypes(iType).field), maxTime);
             fprintf(fid, '%s\t%g\t%g\t%g\t%g\t%g\t%g\n', data(i).name, data(i).id,...
                 data(i).speed_full, data(i).speed_half,...
-                -1/bFit(2), -1/bFit(4), r2);
+                -1/bFit(2), -1/bFit(4), r2(i));
         end
+        r2 = r2(validData);
         
         allInorm = horzcat(data.(dataTypes(iType).field));
         speed_full =  zeros(numel(filters), 1);
@@ -114,18 +119,24 @@ for iType = 1 : numel(dataTypes)
         t2_filt = zeros(numel(filters), 1);
         r2_filt = zeros(numel(filters), 1);
         for iFilter = 1: numel(filters)
+            index{iFilter} = (r2 > filters(iFilter));
             % Calculate speed for the mean positions
-            all_positions_filt = mean(all_positions(:, filters(iFilter).values), 2);
+            all_positions_filt = mean(all_positions(:, index{iFilter}), 2);
             coeff = polyfit(data(1).times, all_positions_filt, 1);
             speed_full(iFilter) = abs(coeff(1)) *60;
             coeff = polyfit(data(1).times(1:end/2), all_positions_filt(1:end/2), 1);
             speed_half(iFilter) = abs(coeff(1)) *60;
             
-            dInorm_filt = mean(allInorm(:, filters(iFilter).values), 2);
-            [bFit, r2] = fitPADecay(data(1).times, dInorm_filt, maxTime);
+%             dInorm_filt = mean(allInorm(:, filters(iFilter).values), 2);
+    times = repmat(data(1).times, 1, sum(index{iFilter}));
+    dInorm_filt = allInorm(:, index{iFilter});
+
+%             [bFit, r2] = fitPADecay(data(1).times, dInorm_filt, maxTime);
+            [bFit, r2Fit, ci] = fitPADecay(times(:), dInorm_filt(:), maxTime);
             t1_filt(iFilter) = -1/bFit(2);
             t2_filt(iFilter) = -1/bFit(4);
-            r2_filt(iFilter) = r2;
+            r2_filt(iFilter) = r2Fit;
+            [bFit(1) bFit(3)]
         end
         
         
@@ -134,15 +145,15 @@ for iType = 1 : numel(dataTypes)
             'Half-range speed (microns/min)\t'...
             'Fast turnover time (s)\tSlow turnover time (s)\tr2\n']);
         for i = 1 : numel(filters)
-            fprintf(fid, '%s\t%s\t%g\t%g\t%g\t%g\t%g\n', filters(i).name,...
-                sum(filters(i).values), speed_full(i), speed_half(i),...
+            fprintf(fid, '%s\t%g\t%g\t%g\t%g\t%g\t%g\n', filters(i),...
+                nansum(index{i}), speed_full(i), speed_half(i),...
                 t1_filt(i), t2_filt(i), r2_filt(i));
         end
         fclose(fid);
         
         % Upload results file to OMERO
-        uploadFileResults(session, resultsPath, 'dataset', datasetId,...
-            [ns '.result.' dataTypes(iType).name '.' num2str(maxTime)]);
+%         uploadFileResults(session, resultsPath, 'dataset', datasetId,...
+%             [ns '.result.' dataTypes(iType).name '.' num2str(maxTime)]);
     end
 end
 
--- photoactivation/fitPADecay.m
+++ photoactivation/fitPADecay.m
@@ -1,4 +1,4 @@
-function [bFit, r2] = fitPADecay(times, dI, varargin)
+function [bFit, r2, ci] = fitPADecay(times, dI, varargin)
 
 % Parse input range
 ip = inputParser;
@@ -7,14 +7,14 @@ ip.parse(varargin{:});
 range = times <= ip.Results.maxTime;
 
 %Double-exponential function for fitting
-turnoverFn = @(b,x)(b(1) .* exp(b(2) .* x))+(b(3) .* exp(b(4) .* x)) +b(5);
+turnoverFn = @(b,x)(b(1) .* exp(b(2) .* x))+(b(3) .* exp(b(4) .* x)) + (1-b(1)-b(3));
 diffFn = @(p) turnoverFn(p, times(range)) - dI(range);
 
 
 % Initial parameters
-bInit = [.8 -.1 .2 -0.01 0];
-lb = [0 -Inf 0 -Inf 0];
-ub = [1.2 0 1.2 0 .2];
+bInit = [.8 -.1 .2 -0.01];
+lb = [0 -Inf 0 -Inf];
+ub = [1.2 0 1.2 0];
 
 % Define fitting options
 opts = optimset('MaxFunEvals', 1e4, ...
@@ -24,6 +24,12 @@ opts = optimset('MaxFunEvals', 1e4, ...
     'Tolfun', 1e-8);
 
 % Use non-linear least squares
-[bFit, resnorm] = lsqnonlin(diffFn, bInit, lb, ub, opts);
+[bFit, resnorm,r, ~,~,~, J] = lsqnonlin(diffFn, bInit, lb, ub, opts);
+% [bFit, resnorm, r, ~, ~, J] =  lsqcurvefit(...
+%     turnoverFn, bInit, times(range), dI(range));
+% resnorm=sum(r.^2);
 r2 = 1 - resnorm / norm(dI-mean(dI))^2;
+
+
+ci = nlparci(bFit,r,'Jacobian',J);
 
