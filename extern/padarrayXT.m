0b7e29183498e2c2d1cb212662667fcf693b5d3f
Sebastien Besson <seb.besson@gmail.com>
2014-08-25 12:46:52 +0100

    Add licensing information

--- extern/padarrayXT.m
+++ extern/padarrayXT.m
@@ -60,6 +60,25 @@ function b = padarrayXT(varargin)
 %       D = padarray(C,[3 3],0,'both')
 %
 %   See also CIRCSHIFT, IMFILTER.
+%
+% Copyright (C) 2014 LCCB 
+%
+% This file is part of u-track.
+% 
+% u-track is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% u-track is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with u-track.  If not, see <http://www.gnu.org/licenses/>.
+% 
+% 
 
 %   Copyright 1993-2010 The MathWorks, Inc.
 %   $Revision: 1.11.4.13 $  $Date: 2011/08/09 17:51:33 $
--- extern/perpVector.m
+++ extern/perpVector.m
@@ -13,6 +13,25 @@ function [v_perp v_proj]=perpVector(point1,e_1,point2)
 %
 %c: 1/03 Jonas
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Copyright (C) 2014 LCCB 
+%
+% This file is part of u-track.
+% 
+% u-track is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% u-track is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with u-track.  If not, see <http://www.gnu.org/licenses/>.
+% 
+% 
 
 %============================
 % TEST INPUT
